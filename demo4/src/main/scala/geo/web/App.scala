8c73d682cc93e7495cd97a70bec1c2e5b01d4fec
majin <majin139@126.com>
2018-11-26 15:52:18 +0800

    pom for demo4

---
+++ demo4/src/main/scala/geo/web/App.scala
@@ -0,0 +1,28 @@
+/*
+ * Copyright (c) GeoWay All Rights Reserved. 2018.9.25.
+ * 项目名称：MajinIdelScala
+ * 工程名称：demo4
+ * 文件名称：App.scala
+ * 开始日期:18-9-25 下午5:08
+ * Author:马锦
+ * Version:1.0.0
+ * 文件路径:D:/MajinIdelScala/demo4/src/main/scala/geo/web/App.scala
+ * 修改日期：18-9-19 上午11:30
+ *
+ */
+
+package geo.web
+
+/**
+ * @author ${user.name}
+ */
+object App {
+  
+  def foo(x : Array[String]) = x.foldLeft("")((a,b) => a + b)
+  
+  def main(args : Array[String]) {
+    println( "Hello World!" )
+    println("concat arguments = " + foo(args))
+  }
+
+}
