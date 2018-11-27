ca01ba101553c98800c382ba08fa06d19d7dc4b7
majin <majin139@126.com>
2018-11-27 11:36:56 +0800

    pom for demo1

---
+++ demo7/src/main/scala/geo/plugin/App.scala
@@ -0,0 +1,34 @@
+
+
+
+
+/*
+ * Copyright (c) GeoWay All Rights Reserved. 2018.9.25.
+ * 项目名称：MajinIdelScala
+ * 工程名称：demo7
+ * 文件名称：App.scala
+ * 开始日期:18-9-25 下午5:08
+ * Author:马锦
+ * Version:1.0.0
+ * 文件路径:D:/MajinIdelScala/demo7/src/main/scala/geo/plugin/App.scala
+ * 修改日期：18-9-25 下午5:08
+ *
+ */
+
+
+package geo.plugin
+
+
+
+object App {
+
+  def main(args : Array[String]): Unit = {
+    Predef.println( "Hello World!" )
+    Predef.println("concat arguments = " )
+    Predef.println("wqeqweqwewe")
+    Predef.println("wqeqweqwewe")
+
+  }
+
+
+}
